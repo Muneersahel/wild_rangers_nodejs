d0916c6d68c09f94bc30e1438d08a3e24dad57b8
benfild <fildben@gmail.com>
2022-06-07 23:16:46 -0400

    multiple changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,10 +4,6 @@ exports.getDashboard = (req, res, next) => {
     res.render("dashboard");
 };
 
-exports.getLogin = (req, res, next) => {
-    res.render("login");
-};
-
 exports.getRanger = (req, res, next) => {
     res.render("ranger-detals");
 };
